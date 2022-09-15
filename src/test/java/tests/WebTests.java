e7fa6d6853480e335467819e7874336c5bbf291e
Vladimir Parfionov <jobstest18@gmail.com>
2022-09-15 12:45:02 +0400

    commit

--- src/test/java/tests/WebTests.java
+++ src/test/java/tests/WebTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class WebTests extends TestBase {
+public class WebTests {
     WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
     @Test
